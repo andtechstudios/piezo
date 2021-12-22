0a204a847db127da421ff0bdbbfe53db37338968
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-22 11:55:02 -0800

    fix scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
-/mnt/c/users/andrew/Desktop/piezo-work/piezo/tools/build.sh
+#!/usr/bin/env bash
+
+set -e
+
+buildDir=$(realpath $1)
+scriptsDir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+rootDir=$(realpath $scriptsDir)
+
+cd $rootDir
+scripts/frontmatter.csx site/content
+scripts/home.csx site/content
+hugo-obsidian -input=site/content -output=site/data -index=true 1> /dev/null
+scripts/list.csx site/content
+hugo --source site --destination $buildDir
