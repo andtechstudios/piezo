6d9ebd00474e2ddc3c5cd65904fcca088bd8e0ca
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-23 23:23:45 -0800

    fix script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ source=$(realpath $1)
 destination=$(realpath $2)
 
 $root/scripts/frontmatter.csx $source/content
-$root/home.csx $source/content
+$root/homepage.csx $source/content
 hugo-obsidian -input=$source/content -output=$source/data -index=true 1> /dev/null
 $root/scripts/list.csx site/content
 hugo --source $source --destination $destination
