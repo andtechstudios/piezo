5cbf3ffab2be942052c64602f4621083825ddd56
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-23 23:28:56 -0800

    fix ci

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ source=$(realpath $1)
 destination=$(realpath $2)
 
 $root/scripts/frontmatter.csx $source/content
-$root/homepage.csx $source/content
+$root/scripts/homepage.csx $source/content
 hugo-obsidian -input=$source/content -output=$source/data -index=true 1> /dev/null
 $root/scripts/list.csx $source/content
 hugo --source $source --destination $destination --minify
