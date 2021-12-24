747ffc8a1a11cca43fec6a40ea135a07a5336949
Andrew <8265381-andtech@users.noreply.gitlab.com>
2021-12-23 23:23:04 -0800

    fix build script

--- build.sh
+++ build.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-buildDir=$(realpath $1)
-scriptsDir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
-rootDir=$(realpath $scriptsDir)
+root=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+source=$(realpath $1)
+destination=$(realpath $2)
 
-cd $rootDir
-scripts/frontmatter.csx site/content
-scripts/home.csx site/content
-hugo-obsidian -input=site/content -output=site/data -index=true 1> /dev/null
-scripts/list.csx site/content
-hugo --source site --destination $buildDir
+$root/scripts/frontmatter.csx $source/content
+$root/home.csx $source/content
+hugo-obsidian -input=$source/content -output=$source/data -index=true 1> /dev/null
+$root/scripts/list.csx site/content
+hugo --source $source --destination $destination
